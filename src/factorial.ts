f0f23841b361889335086c5189de3f38ecd0d58a
Tomáš Chovančák <xchovanc@fi.muni.cz>
2024-09-23 13:31:17 +0200

    feat: introduce incorrect UI output to fail E2E tests

--- src/factorial.ts
+++ src/factorial.ts
@@ -5,5 +5,5 @@ export function factorial(n:number):number {
 }
 
 export function initFactorialUi(component: string) { // Type error: component should be HTMLElement
-  component.innerHTML = `Factorial value <code>5!</code> is <code>${factorial(5)}</code>.`; // Error: 'string' does not have 'innerHTML'
+  component.innerHTML = `Factorial value <code>5!</code> is <code>${factorial(3)}</code>`; // Incorrectly display factorial(3) instead of factorial(5)
 }
